00b6c301886bda418b7504f200b808b6ee960f03
iramirez <iramirez@atistemas.com>
2018-02-22 10:05:30 +0100

    Create Watch gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,17 @@
-var gulp = require('gulp');
-var lint = require('gulp-csslint');
+var gulp = require("gulp");
+var csslint = require("gulp-csslint");
+
+gulp.task("css", function() {
+    gulp
+        .src("example.css")
+        .pipe(csslint())
+        .pipe(csslint.formatter())
+        .on('end', function() {
+            console.log('\n--------\nAnálisis Finalizado\n--------\n');
+        });
+});
+
+
+gulp.task('default', function() {
+    gulp.watch('./example.css', ['css']);
+});
